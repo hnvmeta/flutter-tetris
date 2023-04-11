ce96e29243efbd1421300e12aaa4fc1559c873e1
HungNV <hnv248@gmail.com>
2023-04-11 11:12:41 +0700

    Crate tetris game

---
+++ android/app/src/main/kotlin/com/appbit/tetris_game/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.appbit.tetris_game
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:tetris_game/gamer/gamer.dart';
+import 'package:tetris_game/generated/l10n.dart';
+import 'package:tetris_game/material/audios.dart';
+import 'package:tetris_game/panel/page_portrait.dart';
+
+import 'gamer/keyboard.dart';
+
+void main() {
+  debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
+  _disableDebugPrint();
+  runApp(const MyApp());
+}
+
+void _disableDebugPrint() {
+  bool debug = false;
+  assert(() {
+    debug = true;
+    return true;
+  }());
+  if (!debug) {
+    debugPrint = (message, {wrapWidth}) {
+      //disable log print when not in debug mode
+    };
+  }
+}
+
+final RouteObserver<ModalRoute> routeObserver = RouteObserver<ModalRoute>();
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'tetris',
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate
+      ],
+      navigatorObservers: [routeObserver],
+      supportedLocales: S.delegate.supportedLocales,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: Sound(child: Game(child: KeyboardController(child: _HomePage()))),
+      ),
+    );
+  }
+}
+
+const SCREEN_BORDER_WIDTH = 3.0;
+
+const BACKGROUND_COLOR = Color(0xffefcc19);
+
+class _HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //only Android/iOS support land mode
+    bool land = MediaQuery.of(context).orientation == Orientation.landscape;
+    return land ? PageLand() : PagePortrait();
+  }
+}
